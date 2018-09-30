ca364c0a48bc4c1c82844b4242c9e4077f37fda1
Birsol <36923015+Birsol@users.noreply.github.com>
2018-09-30 09:21:27 +0200

    Add files via upload

--- source/commands/create.js
+++ source/commands/create.js
@@ -5,16 +5,16 @@ module.exports = class extends Command {
     constructor(client, filePath) {
         super(client, filePath, {
             name: "create",
-            aliases: ["start", "begin"]
+            aliases: ["start", "creer"]
         });
     }
 
     async execute(message) {
-        if (!message.channel.permissionsFor(message.guild.me).has(['EMBED_LINKS', 'ADD_REACTIONS'])) return message.channel.send('Invalid Permissions. For simplicity, `EMBED_LINKS` and `ADD_REACTIONS` permissions are required.');
+        if (!message.channel.permissionsFor(message.guild.me).has(['EMBED_LINKS', 'ADD_REACTIONS'])) return message.channel.send('Permissions invalides. Pour faire simple, les permissions `EMBED_LINKS` et `ADD_REACTIONS` sont requises.');
         if (!message.guildAdmin && !message.globalAdmin) return message.channel.send('Invalid Permissions. `MANAGE_SERVER` permission or `MG Admin` role required.');
 
         const match = /(?:start|begin|create)(?:\s+<?#?(\d{17,20})>?)?(?:\s+-w\s+(\d+))?\s+(?:(\d+)|(?:(\d+)\s*d(?:ays)?)?\s*(?:(\d+)\s*h(?:ours|rs|r)?)?\s*(?:(\d+)\s*m(?:inutes|in)?)?\s*(?:(\d+)\s*s(?:econds|ec)?)?)\s+(.+)/i.exec(message.content);
-        if (!match) return message.channel.send(`Invalid Command Usage: \`${this.client.config.prefix}create [channel-mention|channel-id][\'-w <winner-count>\'] <#d#h#m#s|time-in-seconds> <giveaway-title>\``);
+        if (!match) return message.channel.send(`Usage de la commande invalide : \`${this.client.config.prefix}create [#|nom-du-salon][\'-w <nombre de gagnants>\'] <#d#h#m#s|temps-en-secondes> <titre-du-giveaway>\``);
 
         const channel = message.guild.channels.get(match[1]) || message.channel;
 
@@ -24,13 +24,13 @@ module.exports = class extends Command {
 
         const filtered = this.client.giveawayCache.filter(giveaway => giveaway.channel.id === (match[1] ? match[1] : message.channel.id)).size;
 
-        if (filtered >= 5) return message.channel.send(`ERR: You may only have a maximum of 5 giveaways per channel.`);
+        if (filtered >= 5) return message.channel.send(`ERREUR: Vous devez au maximum avoir 5 giveaways sur les salons`);
 
         /*Object.keys(this.client.giveawayCache).forEach(key => key.includes(match[1] ? message.guild.channels.get(match[1]) : message.channel.id) ? filtered.push(key) : null);
 
         if(filtered.length > 5) return message.channel.send(`ERR: You may only have a maximum of 5 giveaways per channel.`);*/
 
-        if (message.deletable) message.delete({ timeout: 1000, reason: "Giveaway Bot" });
+        if (message.deletable) message.delete({ timeout: 1000, reason: "M. V-Bucks" });
 
         new Giveaway(this.client, message, match);
     }
--- source/commands/delete.js
+++ source/commands/delete.js
@@ -4,24 +4,24 @@ module.exports = class extends Command {
     constructor(client, filePath) {
         super(client, filePath, {
             name: "delete",
-            aliases: ["stop", "end"]
+            aliases: ["arret", "end"]
         });
     }
 
     execute(message) {
-        if(!message.guildAdmin && !message.globalAdmin) return message.channel.send('Invalid Permissions. `MANAGE_SERVER` permission or `MG Admin` role required.');
+        if(!message.guildAdmin && !message.globalAdmin) return message.channel.send('Permissions invalides. La permission `MANAGE_SERVER` ou le rôle `MG Admin` est requis.');
 
         const match = /(?:delete|stop|end)(?:\s+(?:<#)?(\d{17,20})(?:>)?)?(?:\s+(\d+))/i.exec(message.content);
 
-        if (!match) return message.channel.send(`Invalid Command Usage: \`${this.client.config.prefix}delete [channel-mention|channel-id] <giveaway-number>\``);
+        if (!match) return message.channel.send(`Usage de la commande invalide : \`${this.client.config.prefix}delete [#|nom-du-salon] <nombre-du-giveaway>\``);
 
         const channel = match[1] ? message.guild.channels.get(match[1]) || message.channel : message.channel;
 
-        if (this.client.giveawayCache.filter(giveaway => giveaway.channel.id === channel.id).size <= 0) return message.channel.send(`There is no currently running giveaway in **${channel.name}**.`);
+        if (this.client.giveawayCache.filter(giveaway => giveaway.channel.id === channel.id).size <= 0) return message.channel.send(`Il n'y a pas de giveaway en cours dans le salon **${channel.name}**.`);
 
         const a = this.client.giveawayCache.get(`${channel.id}-${match[2]}`);
 
-        if(!a) return message.channel.send(`ERR: No giveaway is occuring in the specified channel with that ID.`);
+        if(!a) return message.channel.send(`ERREUR: Il n'y a pas de giveaway en cours dans ce salon !`);
 
         clearInterval(a.interval);
 
@@ -29,6 +29,6 @@ module.exports = class extends Command {
 
         this.client.giveawayCache.delete(`${channel.id}-${match[2]}`);
 
-        message.channel.send(`Giveaway **${match[2]}** has been deleted.`);
+        message.channel.send(`Le giveaway **${match[2]}** a été supprimé`);
     }
 };
